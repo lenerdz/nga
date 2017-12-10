c681468648c2145d2d77a3b7aa22580fd15780c0
Nerd <gerardoccf@gmail.com>
2017-12-10 05:43:12 -0300

    First test

---
+++ www/js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $(".button-collapse").sideNav({
+        menuWidth: 200
+    });
+    var $toastContent = $('<span>I am toast content</span>').add($('<button class="btn-flat toast-action">Undo</button>'));
+    Materialize.toast($toastContent, 10000);
+    //$('.tap-target').tapTarget('open');
+    $('ul.tabs').tabs({
+        swipeable: true,
+    });
+});
